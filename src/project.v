87ecf746c27ad3566f4fbc19b9d05c9128e067e0
priyankaa2302 <priyankaa2302@gmail.com>
2026-05-13 11:32:45 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 `default_nettype none
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
 module tt_um_updowncounter (
     input  wire [7:0] ui_in,
@@ -12,22 +12,22 @@ module tt_um_updowncounter (
     input  wire       rst_n
 );
 
-  reg [7:0] count;
+    reg [7:0] count;
 
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n)
-      count <= 8'd0;
-    else if (ena) begin
-      if (ui_in[0])
-        count <= count + 1'b1;
-      else
-        count <= count - 1'b1;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            count <= 8'd0;
+        else if (ena) begin
+            if (ui_in[0])
+                count <= count + 1'b1;
+            else
+                count <= count - 1'b1;
+        end
     end
-  end
 
-  assign uo_out  = count;
-  assign uio_out = 8'd0;
-  assign uio_oe  = 8'd0;
+    assign uo_out  = count;
+    assign uio_out = 8'd0;
+    assign uio_oe  = 8'd0;
 
 endmodule
 
