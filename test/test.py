e6e960efd0089c070f4d8a47315b064b8db551b0
priyankaa2302 <priyankaa2302@gmail.com>
2026-05-13 11:33:36 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,54 +1,38 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge
 
 
 @cocotb.test()
 async def test_updowncounter(dut):
-    dut._log.info("Start Up/Down Counter Test")
 
-    # Clock: 10 us period (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, unit="ns")
     cocotb.start_soon(clock.start())
 
-    # --------------------
-    # RESET
-    # --------------------
-    dut._log.info("Reset")
+    dut.rst_n.value = 0
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
 
-    await ClockCycles(dut.clk, 5)
+    for _ in range(5):
+        await RisingEdge(dut.clk)
+
     dut.rst_n.value = 1
 
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0, "Reset failed"
+    await RisingEdge(dut.clk)
 
-    # --------------------
-    # UP COUNT TEST
-    # ui_in[0] = 1 → count up
-    # --------------------
-    dut._log.info("Counting UP")
+    assert int(dut.uo_out.value) == 0
+
+    # UP count
     dut.ui_in.value = 1
 
     for i in range(1, 6):
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i, f"UP count failed at {i}"
+        await RisingEdge(dut.clk)
+        assert int(dut.uo_out.value) == i
 
-    # --------------------
-    # DOWN COUNT TEST
-    # ui_in[0] = 0 → count down
-    # --------------------
-    dut._log.info("Counting DOWN")
+    # DOWN count
     dut.ui_in.value = 0
 
-    for i in range(5, 0, -1):
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i - 1, f"DOWN count failed at {i}"
-
-    dut._log.info("Test completed successfully")
+    for i in range(4, -1, -1):
+        await RisingEdge(dut.clk)
+        assert int(dut.uo_out.value) == i
